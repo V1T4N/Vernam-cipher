cc4c5bc1eb0c6ebaf3bb5cf4eaca341c23e44113
YuzukiMIMURA <yuzuki725.m@gmail.com>
2018-01-27 14:11:38 +0900

    add comment

--- encrypt.py
+++ encrypt.py
@@ -10,24 +10,22 @@ message_ascii = [""]*len(message)
 message_bit = [""]*len(message)
 
 
-for i in range (0,len(message)):
+for i in range (0,len(message)):                    #入力された文字をASCIIコード化し，さらにそれを２進数に変換してリストに格納する
     message_ascii[i] = ord(message_list[i])
-    message_bit[i] = format(message_ascii[i],'08b')
+    message_bit[i] = format(message_ascii[i],'08b') 
 
 
-message_bit_temp = "".join(message_bit)
+message_bit_temp = "".join(message_bit)             #入力された文字の２進数リストを結合する
 
-bit_number = "0" + str(len(message)*8) +"b"
+bit_number = "0" + str(len(message)*8) +"b" #入力された文字のビット数の取得
 
-key = random.randint(10000, 10000)
-key_bit = format (key,bit_number)
+key = random.randint(0, 2**(len(message)*8)) #復号のための鍵は入力された文字数分のビットの最大値以下でランダムに生成される
+key_bit = format (key,bit_number)            #鍵を２人数に変換
 
-message_temp = int(message_bit_temp,2)
-
-
+message_temp = int(message_bit_temp,2)      #結合した２進数表記の文字列を10進数に変換する
 
 
-#print "message"+ message_bit_temp
+
 
 
 
@@ -37,23 +35,24 @@ print "Your key is [" +key_bit + "]"
 print "Your message is " + str(len(message)*8) + "bits" 
 
 
-encrypted_message = int(format (message_temp ^ key,bit_number),2)
+encrypted_message = int(format (message_temp ^ key,bit_number),2)   #入力された文字を排他的論理和によって暗号化する．　復号のために10進数の整数型に直しておく
 
-decrypted_message = format(encrypted_message ^ key,bit_number)
+decrypted_message = format(encrypted_message ^ key,bit_number)      #復号
 
 decrypted_message_list_bit = [decrypted_message[i: i+8] for i in range(0, len(decrypted_message), 8)]
+#復号された文字は2進数表記なので８ビットごとに分けてリストに格納する．
 
 
 decrypted_message_list = [""]*len(message)
-decrypted_message_list_ascii = [""]*len(message)
+decrypted_message_list_ascii = [""]*len(message) #リストの定義
 
-for i in range (0,len(message)):
+for i in range (0,len(message)):                                        ##復号された2進数表記のリストをASCII文字に直す
     decrypted_message_list[i] = int(decrypted_message_list_bit[i],2)
     decrypted_message_list_ascii[i] = chr(decrypted_message_list[i])
 
 
 
-ans = "".join(decrypted_message_list_ascii)
+ans = "".join(decrypted_message_list_ascii)         ##リストを結合する
 
 print "." +"\n"
 print "Decrypting..." +"\n"
