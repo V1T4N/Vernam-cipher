be2a2913fbdafacbe796d7063ef332da0e7e029e
YuzukiMIMURA <yuzuki725.m@gmail.com>
2018-01-29 08:33:14 +0900

    fix

--- encrypt.py
+++ encrypt.py
@@ -11,16 +11,16 @@ message_bit = [""]*len(message)
 
 
 for i in range (0,len(message)):                    #入力された文字をASCIIコード化し，さらにそれを２進数に変換してリストに格納する
-    message_ascii[i] = ord(message_list[i])
-    message_bit[i] = format(message_ascii[i],'08b') 
+    message_ascii[i] = ord(message_list[i])         #10進数のACSIIコード化
+    message_bit[i] = format(message_ascii[i],'08b') #2進数に変換
 
 
 message_bit_temp = "".join(message_bit)             #入力された文字の２進数リストを結合する
 
-bit_number = "0" + str(len(message)*8) +"b" #入力された文字のビット数の取得
+bit_number = "0" + str(len(message)*8) +"b" #入力された文字のビット数の取得(0b{n}の形)
 
 key = random.randint(0, 2**(len(message)*8)) #復号のための鍵は入力された文字数分のビットの最大値以下でランダムに生成される
-key_bit = format (key,bit_number)            #鍵を２人数に変換
+key_bit = format (key,bit_number)            #鍵を二進数に変換
 
 message_temp = int(message_bit_temp,2)      #結合した２進数表記の文字列を10進数に変換する
 
@@ -35,7 +35,23 @@ print "Your key is [" +key_bit + "]"
 print "Your message is " + str(len(message)*8) + "bits" 
 
 
+
+
+
+
 encrypted_message = int(format (message_temp ^ key,bit_number),2)   #入力された文字を排他的論理和によって暗号化する．　復号のために10進数の整数型に直しておく
+
+
+print "Your encrypted message is[" + format (message_temp ^ key,bit_number) + "]" ##表示(2進数)
+print "Your key is [" +key_bit + "]"
+
+print "Your message is " + str(len(message)*8) + "bits" 
+
+
+
+
+
+
 
 decrypted_message = format(encrypted_message ^ key,bit_number)      #復号
 
